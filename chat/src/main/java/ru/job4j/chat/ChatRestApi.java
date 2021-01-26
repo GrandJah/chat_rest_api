540444e19bd19d9bc1fd10dc42bb95d1ad027814
CrandJah <bujhtitxrf@gmail.com>
2021-01-26 17:45:56 +1100

    create module "chat"

---
+++ chat/src/main/java/ru/job4j/chat/ChatRestApi.java
@@ -0,0 +1,13 @@
+package ru.job4j.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ChatRestApi {
+  public static void main(String[] args) {
+    SpringApplication.run(ChatRestApi.class, args);
+  }
+}
